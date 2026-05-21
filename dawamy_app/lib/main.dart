947cfe93001c4cc6d90634ec4841665ab8a590e7
Yousra Mostafa Azmy <54082@ModernGas.local>
2026-05-21 09:41:45 +0300

    Add google-services.json, remove firebase_options.dart

--- dawamy_app/lib/main.dart
+++ dawamy_app/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'app.dart';
 import 'core/constants/app_constants.dart';
 
@@ -44,7 +43,7 @@ void main() async {
   }
 
   try {
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    await Firebase.initializeApp();
   } catch (e) {
     debugPrint('Firebase initialization skipped: $e');
   }
